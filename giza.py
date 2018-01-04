31bc49ae813bfed7199f5d130924f41ecc96058c
jmorrise <jesslm83@gmail.com>
2018-01-03 23:43:27 -0800

    added retries and default time

--- giza.py
+++ giza.py
@@ -14,7 +14,7 @@ LUBY_BAY_PARK_ID = 70473
 LENGTH_OF_STAY = 1
 ARRIVAL_DATE = "6/17/2018"
 
-SITE_IDS = {50:1845}
+SITE_IDS = {50:1845, 1:1890, 18:1867, 22:1877, 24:1889}
 
 def get_login_payload(email, password):
 	payload = {"AemailGroup_1733152645":email,
@@ -42,9 +42,9 @@ def write_response_html_to_file(response, filename="output.html"):
 
 def extract_num_items_in_cart(soup_html):
 	shop_cart_link_text = soup_html.find("a", {"id":"cartLink"}).text
-	print(shop_cart_link_text + " is the shopping cart link text")
+	print("'{}' is the shopping cart link text.".format(shop_cart_link_text))
 	num_items = int(shop_cart_link_text[-1])
-	print("There are {} items in the cart".format(num_items))
+	print("There are {} items in the cart.".format(num_items))
 	return num_items
 
 class GizaBot:
@@ -57,6 +57,7 @@ class GizaBot:
 		self.site = None
 		self.date = None
 		self.length = None
+		self.retries = None
 
 	def set_time(self, hour, minute, second=0):
 		self.has_time = True
@@ -72,6 +73,9 @@ class GizaBot:
 
 	def set_length_of_stay(self, length):
 		self.length = length
+
+	def set_retries(self, retries):
+		self.retries = retries
 
 	def wait(self):
 		now = datetime.now()
@@ -89,6 +93,9 @@ class GizaBot:
 			now = datetime.now()
 
 	def book_site(self, email, password):
+		if self.retries < 0:
+			raise ValueError("Retries must be > 0")
+
 		with requests.Session() as s:
 			login_payload = get_login_payload(email, password)
 			login_response = s.post(LOGIN_URL, login_payload)
@@ -101,21 +108,25 @@ class GizaBot:
 				self.wait()
 
 			# Book the site
-			booking_response = s.post(BOOKING_URL, booking_payload)
+			for i in range(self.retries):
 
-			if (booking_response.status_code != 200):
-				raise Exception("failedRequest","ERROR, %d code received".format(booking_response.status_code))
+				booking_response = s.post(BOOKING_URL, booking_payload)
 
-			html = BeautifulSoup(booking_response.text, 'html.parser')
-			num_items = extract_num_items_in_cart(html)
-			if num_items == 1:
-				print("Site reserved")
-			else:
-				print("Not reserved")
+				print("attempted at {}".format(str(datetime.now())))
+				if (booking_response.status_code != 200):
+					raise Exception("failedRequest","ERROR, %d code received".format(booking_response.status_code))
 
-			write_response_html_to_file(booking_response, "output2.html")
+				html = BeautifulSoup(booking_response.text, 'html.parser')
+				num_items = extract_num_items_in_cart(html)
+				if num_items == 1:
+					print("Site reserved!")
+					break # don't need to retry
+				else:
+					print("Not reserved")
 
-			
+				
+				
+			# write_response_html_to_file(booking_response, "output2.html")		
 
 
 if __name__ == "__main__":
@@ -124,11 +135,14 @@ if __name__ == "__main__":
 	parser.add_argument("-s", "--site")
 	parser.add_argument("-d", "--date")
 	parser.add_argument("-l", "--length", default="14")
+	parser.add_argument("-r", "--retries", default="1")
 	parser.add_argument("-hr", "--hour")
 	parser.add_argument("-min", "--minute")
 	parser.add_argument("-sec", "--second", default="0")
 	parser.add_argument("-e", "--email")
 	parser.add_argument("-p", "--password")
+
+	parser.add_argument("--use_jordan_default_time", action='store_true')
 
 	args = parser.parse_args()
 	if args.site is None or args.date is None or args.length is None or args.email is None or args.password is None:
@@ -139,13 +153,16 @@ if __name__ == "__main__":
 	giza_bot.set_site(int(args.site))
 	giza_bot.set_date(args.date)
 	giza_bot.set_length_of_stay(args.length)
+	giza_bot.set_retries(int(args.retries))
 
-	if (args.hour is not None) and (args.minute is not None):
+	if args.use_jordan_default_time:
+		# The default time Jordan wants to use: UTC 2:59:55 pm
+		giza_bot.set_time(14, 59, 55)
+	elif (args.hour is not None) and (args.minute is not None):
 		hour = int(args.hour)
 		minute = int(args.minute)
 		second = int(args.second)
 		giza_bot.set_time(hour, minute, second)
-
 
 	giza_bot.book_site(args.email, args.password)
 
