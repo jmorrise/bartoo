fc2c5f15aa158bd06323de31b581f9e68c38dd31
jmorrise <jesslm83@gmail.com>
2018-01-03 22:55:10 -0800

    Added flags, timer, etc. to giza.py and refactored into a class

--- giza.py
+++ giza.py
@@ -1,5 +1,8 @@
+import argparse
 import requests
+import time
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # URLs
 UNIF_URL = "http://www.recreation.gov/unifSearchResults.do"
@@ -8,17 +11,10 @@ BOOKING_URL = "https://www.recreation.gov/switchBookingAction.do?"
 LUBY_FULL_URL = "https://www.recreation.gov/camping/luby-bay/r/campgroundDetails.do?contractCode=NRSO&parkId=70473"
 
 LUBY_BAY_PARK_ID = 70473
-LENGTH_OF_STAY = 2
+LENGTH_OF_STAY = 1
 ARRIVAL_DATE = "6/17/2018"
 
 SITE_IDS = {50:1845}
-
-BOOKING_PAYLOAD = {"contractCode":"NRSO", 
-            "parkId":LUBY_BAY_PARK_ID, 
-            "lengthOfStay":LENGTH_OF_STAY, 
-            "dateChosen":"true",
-            "siteId":SITE_IDS[50],
-            "arvdate":ARRIVAL_DATE}
 
 def get_login_payload(email, password):
 	payload = {"AemailGroup_1733152645":email,
@@ -28,35 +24,128 @@ def get_login_payload(email, password):
 			"sbmtCtrl":"combinedFlowSignInKit"}
 	return payload
 
-def send_request(email, password):
-	with requests.Session() as s:
-		login_payload = get_login_payload(email, password)
-		login_response = s.post(LOGIN_URL, login_payload)
-		s.get(LUBY_FULL_URL)
-		booking_response = s.post(BOOKING_URL, BOOKING_PAYLOAD)
+# Date must be a string in the form of 7/14/2018
+def get_booking_payload(site, date, length):
+	payload = {"contractCode":"NRSO", 
+		"parkId":LUBY_BAY_PARK_ID, 
+		"lengthOfStay":length, 
+		"dateChosen":"true",
+		"siteId":SITE_IDS[site], 
+		"arvdate":date}
+	return payload
 
-		if (booking_response.status_code != 200):
-			raise Exception("failedRequest","ERROR, %d code received".format(booking_response.status_code))
+def write_response_html_to_file(response, filename="output.html"):
+	html = BeautifulSoup(response.text, 'html.parser')
 
-		html = BeautifulSoup(booking_response.text, 'html.parser')
+	with open(filename, "w") as file:
+		file.write(str(html))
 
-		with open("output.html", "w") as file:
-			file.write(str(html))
+def extract_num_items_in_cart(soup_html):
+	shop_cart_link_text = soup_html.find("a", {"id":"cartLink"}).text
+	print(shop_cart_link_text + " is the shopping cart link text")
+	num_items = int(shop_cart_link_text[-1])
+	print("There are {} items in the cart".format(num_items))
+	return num_items
 
-		print(booking_response.headers)
+class GizaBot:
+
+	def __init__(self):
+		self.has_time = False
+		self.hour = None
+		self.minute = None
+		self.second = None
+		self.site = None
+		self.date = None
+		self.length = None
+
+	def set_time(self, hour, minute, second=0):
+		self.has_time = True
+		self.hour = hour
+		self.minute = minute
+		self.second = second
+
+	def set_site(self, site):
+		self.site = site
+
+	def set_date(self, date):
+		self.date = date
+
+	def set_length_of_stay(self, length):
+		self.length = length
+
+	def wait(self):
+		now = datetime.now()
+		target_time = now.replace(hour=self.hour, minute=self.minute, second=self.second, microsecond=0)
+		time_delta = target_time-now
+		time_delta_seconds = time_delta.total_seconds()
+		if time_delta_seconds < 0:
+			raise Exception("Wait time is negative.")
+		if time_delta_seconds > 2:
+			print("Waiting {} seconds".format(time_delta_seconds))
+			time.sleep(time_delta_seconds-2)
+		now = datetime.now()
+		while (now.hour != self.hour) or (now.minute != self.minute) or (now.second != self.second):
+			time.sleep(0)
+			now = datetime.now()
+
+	def book_site(self, email, password):
+		with requests.Session() as s:
+			login_payload = get_login_payload(email, password)
+			login_response = s.post(LOGIN_URL, login_payload)
+			s.get(LUBY_FULL_URL)
+
+			booking_payload = get_booking_payload(self.site, self.date, self.length)
+			print(booking_payload)
+
+			if self.has_time:
+				self.wait()
+
+			# Book the site
+			booking_response = s.post(BOOKING_URL, booking_payload)
+
+			if (booking_response.status_code != 200):
+				raise Exception("failedRequest","ERROR, %d code received".format(booking_response.status_code))
+
+			html = BeautifulSoup(booking_response.text, 'html.parser')
+			num_items = extract_num_items_in_cart(html)
+			if num_items == 1:
+				print("Site reserved")
+			else:
+				print("Not reserved")
+
+			write_response_html_to_file(booking_response, "output2.html")
+
+			
 
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--site")
+	parser.add_argument("-d", "--date")
+	parser.add_argument("-l", "--length", default="14")
+	parser.add_argument("-hr", "--hour")
+	parser.add_argument("-min", "--minute")
+	parser.add_argument("-sec", "--second", default="0")
+	parser.add_argument("-e", "--email")
+	parser.add_argument("-p", "--password")
 
-    parser.add_argument("-site", "--site_number")
-    parser.add_argument("-hr", "--hour")
-    parser.add_argument("-min", "--minute")
-    parser.add_argument("-sec", "--second", default="0")
-    parser.add_argument("-r", "--retries", default="5") #How many times to retry
-    parser.add_argument("-e", "--email")
-    parser.add_argument("-p", "--password")
-    args = parser.parse_args()
-	send_request()
+	args = parser.parse_args()
+	if args.site is None or args.date is None or args.length is None or args.email is None or args.password is None:
+		raise ValueError("Site, date, length, email and password must all be specified.")
+
+	giza_bot = GizaBot()
+
+	giza_bot.set_site(int(args.site))
+	giza_bot.set_date(args.date)
+	giza_bot.set_length_of_stay(args.length)
+
+	if (args.hour is not None) and (args.minute is not None):
+		hour = int(args.hour)
+		minute = int(args.minute)
+		second = int(args.second)
+		giza_bot.set_time(hour, minute, second)
+
+
+	giza_bot.book_site(args.email, args.password)
 
